872876791da67253979d5b2573202c66837e52c0
David Eastcroft <davideastcroft95@gmail.com>
2020-03-14 13:49:47 +0000

    Adds test and function for finding pet by name.

--- pet_shop.rb
+++ pet_shop.rb
@@ -35,3 +35,12 @@ def pets_by_breed(pet_shop, breed)
   end
   return total_breed
 end
+
+def find_pet_by_name(pet_shop, name)
+  for pets in pet_shop[:pets]
+    if pets[:name] == name
+      return pets
+    end
+  end
+  return nil
+end
