1786a8df6fcddb59b069d28b1f09be34fa06ac34
David Eastcroft <davideastcroft95@gmail.com>
2020-03-15 22:31:49 +0000

    Uses increase pets sold function within sell pet to customer function.

--- pet_shop.rb
+++ pet_shop.rb
@@ -84,7 +84,7 @@ end
 
 def sell_pet_to_customer(pet_shop, pet, customer)
   add_pet_to_customer(customer, pet)
-  pet_shop[:admin][:pets_sold] += 1
+  increase_pets_sold(pet_shop, 1)
   remove_customer_cash(customer, pet[:price])
   add_or_remove_cash(pet_shop, pet[:price])
 end
